dffa66c449f8f6f688c524f11b0877f6daa51f2c
nishant-dash <nishant.dash@canonical.com>
2024-01-20 21:36:26 +0400

    updated manual cloud bootstrap

--- modules/local-juju-controller/main.tf
+++ modules/local-juju-controller/main.tf
@@ -16,12 +16,12 @@ resource "null_resource" "node_setup" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOF
-        echo "sudo snap install juju --channel=3.1/stable",
-        echo  "mkdir -p ~/.local/share/juju",
-        echo  "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa_juju",
-        echo  "cat ~/.ssh/id_rsa_juju.pub >> ~/.ssh/authorized_keys",
-        echo  "ssh ubuntu@$(hostname -i | awk '{print $NF}')",
-        echo  "juju bootstrap manual/$(hostname -i | awk '{print $NF}') localhost"
+        echo "sudo snap install juju --channel=3.1/stable"
+        echo "mkdir -p ~/.local/share/juju"
+        echo "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa_juju"
+        echo "cat ~/.ssh/id_rsa_juju.pub >> ~/.ssh/authorized_keys"
+        echo "ssh ubuntu@$(hostname -i | awk '{print $NF}')"
+        echo "juju bootstrap manual/$(hostname -i | awk '{print $NF}') localhost"
 EOF
   }
 }
