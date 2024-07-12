35352204aa4f210d66ab0114ffa9d681c0fe3bcf
Nishant Dash <nishant.dash@canonical.com>
2024-07-12 13:02:13 +0400

    bumped to juju 3.5

--- modules/local-juju-controller/variables.tf
+++ modules/local-juju-controller/variables.tf
@@ -1,7 +1,7 @@
 variable "juju_channel" {
   description = "Channel of juju client and agent to use"
   type        = string
-  default     = "3.1/stable"
+  default     = "3.5/stable"
 }
 
 variable "local_ip" {
